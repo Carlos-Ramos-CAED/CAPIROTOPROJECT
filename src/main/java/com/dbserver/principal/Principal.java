28bb906d13708e5d4f637958776c1c258c2f9707
Dunky CAED <dunkbol@bol.com.br>
2018-07-27 15:06:12 -0300

    criando versao com todos arquivos da pasta capiroto

---
+++ src/main/java/com/dbserver/principal/Principal.java
@@ -0,0 +1,40 @@
+package com.dbserver.principal;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Principal {
+	private WebDriver driver;
+	public WebDriverWait espera;
+
+	@Before
+	public void setUp() throws InterruptedException {
+		WebDriverManager.firefoxdriver().setup();
+		this.driver = new FirefoxDriver();
+		this.driver.manage().window().maximize();
+		this.driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+		this.driver.get("http://www.automationpractice.com");
+		this.espera = new WebDriverWait(driver, 8);
+	}
+
+	@Test
+	public void testMain() throws InterruptedException {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	//sera  q foi
+	
+	
+}
